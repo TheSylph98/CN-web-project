dbbd24eba860b30ad1c3fd6f8df096c51b3d93e6
nghiapi <daonghiabk@gmail.com>
2019-05-18 15:29:24 +0700

    nothing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,13 +21,14 @@ use App\thongbao;
 class HomeController extends Controller
 {
     //
-    public function index(){
+  public function index(){
       return view('index');
     }
 
-    public function Login(){
+  public function Login(){
       return view('page.login');
     }
+
 
     public function LoginAuth(Request $request){
     // Cách sử dụng validator nếu muốn dùng if để kiểm tra lỗi dữ liệu đầu vào trước khi kiểm tra đăng nhập
@@ -67,10 +68,21 @@ class HomeController extends Controller
           ]);
         } else {
             return response()->json([
-              'login' => 'failed',
-              'errors' => 'Incorrect password or email does not exist'
+              'login' => 'success',
+              'user' => [
+                'username' => $user->ten,
+                'email' => $user->email,
+                'password' => $user->password,
+                'address'=> $user->diachi,
+                'phone'=> $user->sodienthoai
+              ]
             ]);
-    }
+          } else {
+              return response()->json([
+                'login' => 'failed',
+                'errors' => 'Incorrect password or email does not exist'
+              ]);
+      }
   }
 }
 
@@ -234,11 +246,7 @@ class HomeController extends Controller
          'errors' => $err[0]
        ]); }
      else {
-       $tk = new taikhoan;
        $tk->users_id = $user->id;
-       $tk->sotaikhoan = $request->sotaikhoan;
-       $tk->nganhang_id = $request->nganhang;
-       $tk->sotien = 5000000;
        $tk->save();
        return response()->json([
          'add_account'=>'true',
@@ -247,12 +255,12 @@ class HomeController extends Controller
        ]);
    }
  }
-   public function GetBank(){
+  public function GetBank(){
      $bank = nganhang::all();
       return response()->json($bank);
    }
 
-   public function GetPhoneBook(){
+  public function GetPhoneBook(){
      $user = Auth::user();
      $user_id = $user->id;
      $friend = array();
@@ -300,35 +308,33 @@ class HomeController extends Controller
   }
 
   public function TransactionHistory(){
-    $user = Auth::user();
-    $id = $user->id;
-
-    $user_account = taikhoan::where('users_id',$id)->get();
-    $list = array();
-
-    //ls nap tien
-    array_push($list,"naptien");
-    $naptien = naptien::where('users_id',$id)->get();
-    array_push($list,$naptien);
-    //ls chuyentien
-    array_push($list,"chuyentien");
-    $chuyentien = chuyentien::where('id_chuyen',$id)->get();
-    array_push($list,$chuyentien);
-    //ls  napthe
-    array_push($list,"napthe");
-    $napthe = napthe::where('Users_id',$id)->get();
-    array_push($list,$napthe);
-    //ls ruttien
-    array_push($list,"ruttien");
-    $ruttien = ruttien::where('users_id',$id)->get();
-    array_push($list,$ruttien);
-    //ls thanhtoan
-    array_push($list,"thanhtoan");
-    $thanhtoan = thanhtoan::where('users_id',$id)->get();
-    array_push($list,$thanhtoan);
-
-    $lsgd = json_encode($list);
-    return response()->json($lsgd);
+    if(Auth::check()){
+      $user = Auth::user();
+      $id = $user->id;
+      $user_account = taikhoan::where('users_id',$id)->get();
+      //ls nap tien
+      $naptien = naptien::where('users_id',$id)->get();
+      //ls chuyentien
+      $chuyentien = chuyentien::where('id_chuyen',$id)->get();
+      //ls  napthe
+      $napthe = napthe::where('Users_id',$id)->get();
+      //ls ruttien
+      $ruttien = ruttien::where('users_id',$id)->get();
+      //ls thanhtoan
+      $thanhtoan = thanhtoan::where('users_id',$id)->get();
+      return response()->json([
+        'naptien' => $naptien,
+        'chuyentien' => $chuyentien,
+        'napthe' => $napthe,
+        'ruttien' => $ruttien,
+        'thanhtoan' => $thanhtoan
+      ]);
+    } else {
+      return response()->json([
+        'trans'=>'error',
+        'message'=>'ban chua dang nhap'
+      ]);
+    }
   }
 
   public function GetBankUser(){
@@ -342,7 +348,6 @@ class HomeController extends Controller
       $nganhang = nganhang::where('id',$ng_id)->get();
       array_push($list_bank,$nganhang[0]);
     }
-    // $bank_u = json_encode($list_bank);
     return response()->json($list_bank);
   }
    else
@@ -382,7 +387,7 @@ class HomeController extends Controller
         'user' => $user
       ]);
     }
-  } else
+  }else
      return response()->json([
       'bank'=>'error',
       'message'=> 'ban chua dang nhap '
@@ -395,4 +400,35 @@ class HomeController extends Controller
     return view('page.napthe',['nhamang' => $nhamang,'user'=>$user]);
   }
 
+  public function GetThanhToan(){
+    if (Auth::check()){
+      $user = Auth::user();
+      $loaihd = loaihoadon::all();
+      return response()->json([
+        'loaihoadon'=> $loaihd
+      ]);
+  }else
+      return response()->json([
+        'thanhtoan'=> 'error',
+        'message'=>'ban chua dang nhap'
+      ]);
+  }
+
+  public function PostThanhToan(Request $request){
+    if(Auth::check()){
+      $user = Auth::user();
+      $validator = Validator::make($request->all(),
+        [
+          'sotien' => 'required'
+        ],
+        [
+          'sotien.required' => 'You have not money!'
+        ]);
+
+    }else
+      return response()->json([
+        'thanhtoan'=>'error',
+        'message'=>'ban chua dang nhap'
+      ]);
+  }
 }
